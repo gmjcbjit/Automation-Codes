a07bac8a3d9ad54d4ff8ac2df38a29d1ff2992cc
Golam Mohabbat Jan Chowdhury <golam.mohabbat@bjitacademy.com>
2023-01-11 10:48:21 +0600

    assignment 1

---
+++ Class1/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void searchword(String browser,String Keyword){
+        WebDriver driver = null;
+        if(browser=="firefox" || browser =="chrome" || browser=="edge") {
+            if (browser.equalsIgnoreCase("chrome")) {
+                driver = new ChromeDriver();
+
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                driver = new FirefoxDriver();
+
+            } else if (browser.equalsIgnoreCase("edge")) {
+                driver = new EdgeDriver();
+            }
+
+            String URL = "https://www.google.com";
+            driver.get(URL);
+            System.out.println("Browser is opened");
+            WebElement search = driver.findElement(By.name("q"));
+            search.sendKeys(Keyword);
+            search.submit();
+            driver.close();
+
+        }else{
+            System.out.println("This browser is not in scope");
+        }
+
+    }
+    public static void main(String[] args) {
+
+      //String browser = "firefox";
+      searchword("safari", "saad");
+
+
+    }
+}
